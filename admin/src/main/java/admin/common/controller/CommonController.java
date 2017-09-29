cf571f588e51e0465842a2ceb92110e4a522d552
jo <jo@192.168.0.4>
2017-09-29 00:47:29 -0700

    Server Restart Script

--- admin/src/main/java/admin/common/controller/CommonController.java
+++ admin/src/main/java/admin/common/controller/CommonController.java
@@ -1,10 +1,10 @@
 package admin.common.controller;
 
 import java.io.File;
-import java.net.URLEncoder;
+
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
+
 import java.util.Map;
 
 import javax.annotation.Resource;
@@ -22,7 +22,6 @@ import admin.common.util.CommonUtils;
 import admin.common.util.StoreUtils;
 import admin.common.util.FileUtils;
 
-import com.froala.editor.Image;
 import com.google.gson.Gson;
 
 @Controller
@@ -34,25 +33,7 @@ public class CommonController {
 	
 	@Resource(name="fileUtils")
     private FileUtils fileUtils;
-	
-	@RequestMapping(value="/common/downloadFile.do")
-	public void downloadFile(CommandMap commandMap, HttpServletResponse response) throws Exception{
-		Map<String,Object> map = commonService.selectFileInfo(commandMap.getMap());
-//		String storedFileName = (String)map.get("STORED_FILE_NAME");
-//		String originalFileName = (String)map.get("ORIGINAL_FILE_NAME");
-//		
-//		byte fileByte[] = FileUtils.readFileToByteArray(new File("C:\\dev\\file\\"+storedFileName));
-//		
-//		response.setContentType("application/octet-stream");
-//		response.setContentLength(fileByte.length);
-//		response.setHeader("Content-Disposition", "attachment; fileName=\"" + URLEncoder.encode(originalFileName,"UTF-8")+"\";");
-//		response.setHeader("Content-Transfer-Encoding", "binary");
-//		response.getOutputStream().write(fileByte);
-		
-		response.getOutputStream().flush();
-		response.getOutputStream().close();
-	}
-	
+
 	/**
 	 * 이미지 업로드
 	 * @param request
